fa81cbaaddc39bb3206bf533be877c0ed7f98a7f
JohnTeoJF <johnteojf+github@gmail.com>
2022-01-18 09:36:35 +0800

    Add Template

---
+++ src/test/java/ssf/bookSearch/BookSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf.bookSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
